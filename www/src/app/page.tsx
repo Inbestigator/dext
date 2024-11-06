ecf7900cfdf8c3044e058b040d30135a46030ac8
Inbestigator <inbestigatoryt@gmail.com>
2024-11-05 20:16:11 -0800

    Updated homepage

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -5,19 +5,19 @@ import { ExternalLink } from "lucide-react";
 export default function Home() {
   return (
     <div className="h-dvh flex flex-col items-center justify-center text-center gap-10 max-w-5xl mx-auto">
-      <h1 className="z-10 text-3xl font-black leading-tight sm:text-7xl sm:leading-tight">
+      <h1 className="z-10 text-[max(36px,min(5vw,72px))] font-black leading-tight sm:leading-tight">
         The best{"  "}
         <Link
           href="https://deno.com/"
           target="_blank"
           rel="noreferrer"
-          className="relative rounded bg-primary px-3 py-1 text-white hover:underline underline-offset-8"
+          className="relative rounded bg-primary px-3 py-1 text-white hover:underline underline-offset-8 hidden sm:inline"
         >
           Deno
         </Link>{" "}
         framework for Discord.js.
       </h1>
-      <p>
+      <p className="hidden sm:block">
         Dext is a powerful Discord.js framework that autogenerates responses for
         {" "}
         <em>static</em>{" "}
@@ -25,22 +25,25 @@ export default function Home() {
         handles slash command creation, event responses, and is easy to use and
         integrate.
       </p>
-      <div className="flex gap-4 *:font-semibold *:text-lg">
-        <Button size="lg" asChild>
+      <p className="sm:hidden">
+        Dext is a powerful Discord.js framework that optimizes bot performance.
+      </p>
+      <div className="flex flex-wrap justify-center gap-4 *:font-semibold *:text-base *:sm:text-lg *:sm:h-11 *:sm:rounded-md *:sm:px-8">
+        <Button asChild>
           <Link href="/docs">Docs</Link>
         </Button>
-        <Button size="lg" variant="outline" asChild>
+        <Button variant="outline" asChild>
           <Link href="https://discord.js.org" target="_blank" rel="noreferrer">
-            Discord.js <ExternalLink className="!size-5" />
+            Discord.js <ExternalLink className="sm:!size-5" />
           </Link>
         </Button>
-        <Button size="lg" variant="outline" asChild>
+        <Button variant="outline" asChild>
           <Link
             href="https://github.com/inbestigator/dext"
             target="_blank"
             rel="noreferrer"
           >
-            GitHub <ExternalLink className="!size-5" />
+            GitHub <ExternalLink className="sm:!size-5" />
           </Link>
         </Button>
       </div>
